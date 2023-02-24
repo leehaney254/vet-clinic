d6872f6c2c9e732a21b701df172599e65b76619f
Leehaney gee <georgeleehaney20@gmail.com>
2023-02-24 11:16:38 +0300

    Feat: Add many to many relationships

--- schema.sql
+++ schema.sql
@@ -14,13 +14,13 @@ ALTER TABLE animals ADD species varchar(200);
 
 CREATE TABLE owners(
   id SERIAL PRIMARY KEY,
-  full_name varchar(200),
+  full_name VARCHAR(200),
   age INT
 );
 
 CREATE TABLE species (
   id SERIAL PRIMARY KEY,
-  name varchar(200)
+  name VARCHAR(200)
 );
 
 ALTER TABLE animals DROP COLUMN species;
@@ -38,3 +38,43 @@ ADD CONSTRAINT fk_owners
 FOREIGN KEY(owners_id)
 REFERENCES owners(id)
 ON DELETE CASCADE;
+
+-- Create a table named pets
+CREATE TABLE vets (
+ id SERIAL PRIMARY KEY,
+ name VARCHAR(200),
+ age INT,
+ date_of_graduation DATE
+);
+
+-- Create a joining between species and vet 
+CREATE TABLE specializations (
+  species_id INT NOT NULL,
+  vets_id INT NOT NULL,
+  PRIMARY KEY(species_id, vets_id),
+  CONSTRAINT fk_species
+   FOREIGN KEY(species_id)
+    REFERENCES species(id)
+     ON UPDATE CASCADE,
+  CONSTRAINT fk_vets
+   FOREIGN KEY(vets_id)
+    REFERENCES vets(id)
+     ON UPDATE CASCADE
+);
+
+
+-- Joining a joining between animals and vets
+CREATE TABLE visits (
+  animals_id INT NOT NULL,
+  vets_id INT NOT NULL,
+  date_of_visits DATE,
+  PRIMARY KEY(animals_id, vets_id, date_of_visits),
+  CONSTRAINT fk_animals
+   FOREIGN KEY(animals_id)
+    REFERENCES animals(id)
+     ON UPDATE CASCADE,
+  CONSTRAINT fk_vets
+   FOREIGN KEY(vets_id)
+    REFERENCES vets(id)
+     ON UPDATE CASCADE
+);
